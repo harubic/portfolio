e40f721f2e362f8c15f406136edfbbe35bf6ffd3
Herdi Apriansyah <haproject99@gmail.com>
2022-02-06 13:43:04 +0700

    menambahkan hero section dan menganimasikannya sekaligus membuat bagian social media(baru tata letak)

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,31 @@
-$(document).ready(function() {
+// when load 
+$(window).on('load', function() {
+    $('.hero-section h1').removeClass('-translate-y-96');
+});
 
-    $('#nav-toogle :checkbox').change(function() {
-        if (this.checked) {
-            $('#navbar ul').removeClass('h-0 overflow-hidden');
-            $('#navbar').removeClass('opacity-0');
-        } else {
-            $('#navbar ul').addClass(' h-0 overflow-hidden');
-            $('#navbar').addClass('opacity-0');
-        }
-    });
+$('#nav-toogle :checkbox').change(function() {
+    if (this.checked) {
+        $('#navbar ul').removeClass('h-0 overflow-hidden');
+        $('#navbar').removeClass('opacity-0');
+    } else {
+        $('#navbar ul').addClass(' h-0 overflow-hidden');
+        $('#navbar').addClass('opacity-0');
+    }
+});
 
-    // navbar scrollspy
-    let navScroll = $('nav').offset().top;
-    $(window).scroll(function() {
-        let scroll = $(this).scrollTop();
+// navbar scrollspy
+let navScroll = $('nav').offset().top;
+$(window).scroll(function() {
+    let scroll = $(this).scrollTop();
 
-        if (scroll < navScroll) {
-            $('nav').removeClass('fixed top-0 bg-slate-300');
-            $('#brand').addClass('absolute opacity-0');
-        } else {
-            $('nav').addClass('fixed top-0 bg-slate-300');
-            $('#brand').removeClass('absolute opacity-0');
-        }
-    });
+    // navbar
+    if (scroll < navScroll) {
+        $('nav').removeClass('fixed top-0 bg-slate-300');
+        $('nav').addClass('relative');
+    } else {
+        $('nav').addClass('fixed top-0 bg-slate-300');
+        $('nav').removeClass('relative');
+    }
+
+
 });
